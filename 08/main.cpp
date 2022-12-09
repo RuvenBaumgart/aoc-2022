43a471920d0d43f0686fdc84072e2747dd6a0e84
ruven.baumgart <ruven.baumgart@gmail.com>
2022-12-09 06:25:18 +0100

    added day 8

--- 08/main.cpp
+++ 08/main.cpp
@@ -9,8 +9,9 @@ struct pos{
 
 bool checkVisibilityLeft(pos p, std::vector<std::vector<int>>& m){
   int left = p.x;
+  int height = m[p.x][p.y];
   while(left > 0){
-    if(m[left - 1][p.y] > m[left][p.y])
+    if(m[left - 1][p.y] >= height)
       return false;
     left--;
   }
@@ -19,8 +20,9 @@ bool checkVisibilityLeft(pos p, std::vector<std::vector<int>>& m){
 
 bool checkVisibilityTop(pos p, std::vector<std::vector<int>>& m){
   int top = p.y;
+  int height = m[p.x][p.y];
   while(top > 0){
-    if(m[p.x][top - 1] > m[p.y][top])
+    if(m[p.x][top - 1] >= height)
       return false;
     top--;
   }
@@ -29,8 +31,9 @@ bool checkVisibilityTop(pos p, std::vector<std::vector<int>>& m){
 
 bool checkVisibilityRigth(pos p, std::vector<std::vector<int>>& m){
   int right = p.x;
-  while(right < m[p.x].size()){
-    if(m[right][p.y] < m[right + 1][p.y])
+  int height = m[p.x][p.y];
+  while(right < m[p.x].size() - 1){
+    if( m[right + 1][p.y] >= height)
       return false;
     right++;
   }
@@ -39,8 +42,9 @@ bool checkVisibilityRigth(pos p, std::vector<std::vector<int>>& m){
 
 bool checkVisibilityBottom(pos p, std::vector<std::vector<int>>& m){
   int bottom = p.y;
-  while(bottom < m.size()){
-    if(m[p.x][bottom] < m[p.x][bottom +1])
+  int height = m[p.x][p.y];
+  while(bottom < m.size() - 1){
+    if(m[p.x][bottom +1] >= height)
       return false;
     bottom++;
   }
@@ -48,7 +52,15 @@ bool checkVisibilityBottom(pos p, std::vector<std::vector<int>>& m){
 }
 
 bool checkVisibility(pos p, std::vector<std::vector<int>>& m){
-  return checkVisibilityTop(p, m) || checkVisibilityLeft(p, m) || checkVisibilityRigth(p,m) || checkVisibilityBottom(p,m);
+  if(checkVisibilityLeft(p, m))
+        return true;
+  if(checkVisibilityTop(p, m))
+        return true;
+  if(checkVisibilityBottom(p, m))
+        return true;
+  if(checkVisibilityRigth(p, m))
+        return true;
+  return false; 
 }
 
 int main(int argc, char** argv){
@@ -62,20 +74,28 @@ int main(int argc, char** argv){
       lines.push_back(line);
     }
 
-    trees.resize(lines.size());
+    trees.resize(lines.size(), std::vector<int>(lines[0].size()));
 
     for(int i = 0; i < lines.size(); i++){
       for(int j = 0; j < lines[0].size(); j++){
-        trees[i].push_back(lines[i][j]);
+        const char num = lines[i][j];
+        trees[i][j] = std::atoi(&num);
       }
     }
 
+
+    
     int visible = (trees.size() * 2) +  (trees[0].size() * 2) - 4 ;
     for(int i = 1; i < trees.size() -1; i++){
-      for(int j = 1; j < trees[0].size() - 1; j++){
-        if(checkVisibility(pos(i,j), trees))
-          visible++;
+      for(int j = 1; j < trees[0].size() -1; j++){
+          if(checkVisibility(pos(i,j), trees)){
+            visible++; 
+            std::cout << "\033[1;31m" << trees[i][j] << "\033[0m";
+          } else {
+            std::cout << trees[i][j];  
+          }
       }
+      std::cout << std::endl;
     }
 
     std::cout << "Puzzel 1: " << visible << std::endl;
